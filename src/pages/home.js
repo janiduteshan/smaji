85dd9cef6d108e52266173f257d219e611bd6602
Sajith Rathnayaka <sajithchamuditha@icloud.com>
2024-03-24 22:08:39 +0530

    Update home.js
    tagline

--- src/pages/home.js
+++ src/pages/home.js
@@ -21,9 +21,7 @@ function Home() {
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Code Snippet Library</h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Unlock Efficiency: Your One-stop Shop for Code Solutions!
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
